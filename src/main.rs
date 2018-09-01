aca06f1f7b9e0d5e2907e2a8a43afdcd67588e6f
Bykov Igor <igorb.dev@websm.io>
2018-09-01 13:47:45 +0300

    add actix-web

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate ipnetwork;
+#[macro_use]
 extern crate serde_json;
 extern crate actix_web;
 
@@ -6,42 +7,57 @@ use ipnetwork::Ipv4Network;
 use std::process::Command;
 use serde_json::{Value, Error};
 
-use actix_web::{http, server, App, Path, Responder};
+use actix_web::{http, server, App, Path, HttpRequest, HttpResponse};
 
 fn main() {
 
-    server::new(
-        || App::new()
-        .route("/{id}/{name}/index.html", http::Method::GET, index))
-        .bind("127.0.0.1:3000").unwrap()
-        .run();
+    server::new(|| {
+
+        App::new()
+            .route("/", http::Method::GET, |_request: HttpRequest| {
+
+                let ip = get_free_ip("lo0").unwrap();
+                let body = json!({
+                    "ip": ip,
+                    "interface": "lo0"
+                });
+
+                HttpResponse::Ok()
+                    .header("content-type", "application/json")
+                    .body(body.to_string())
+
+            })
+
+    }).bind("127.0.0.1:3000").unwrap().run();
 
 }
 
-fn _get_free_ip() {
+fn get_iface_ips(iface: &str) -> Vec<String> {
 
     let output = Command::new("/usr/bin/netstat")
-        .args(&["-I", "lo0", "-4", "-n", "--libxo", "json"])
+        .args(&["-I", iface, "-4", "-n", "--libxo", "json"])
         .output()
         .expect("netstat command failed to start");
 
     let string_data = String::from_utf8_lossy(&output.stdout);
-
     let json_data: Value = serde_json::from_str(&string_data).unwrap();
-
     let interface = json_data["statistics"]["interface"].as_array().unwrap();
-
     let ips = interface.iter()
         .map(|object| {
 
             println!("{:?}", object["address"]);
-            object["address"].as_str().unwrap()
+            object["address"].as_str().unwrap().to_string()
 
         })
-        .collect::<Vec<&str>>();
+        .collect::<Vec<String>>();
 
-    println!("{:?}", ips);
+    ips
 
+}
+
+fn get_free_ip(iface: &str) -> Option<String> {
+
+    let ips = get_iface_ips(iface);
     let ip = "127.0.0.1".parse().unwrap();
     let network = Ipv4Network::new(ip, 8).unwrap();
     let broadcast = network.broadcast();
@@ -52,11 +68,11 @@ fn _get_free_ip() {
             return false;
         }
 
-        if ips.contains(&ip.to_string().as_str()) { false }
+        if ips.contains(&ip.to_string()) { false }
         else { true }
 
     });
 
-    println!("free: {}", free_ip.unwrap());
+    Some(free_ip.unwrap().to_string())
 
 }
